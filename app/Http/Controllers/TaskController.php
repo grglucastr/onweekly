7fb5de7ee2936cf75294a9a0633774d5ac5a4cb9
George Lucas T. Bentes <george.bentes@gmail.com>
2019-02-21 12:50:53 -0400

    Added methods to handle the routes requests

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,10 +2,40 @@
 
 namespace App\Http\Controllers;
 
-use App\Technician;
+use App\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    
+    public function showTasks()
+    {
+        return response()->json(Task::where('active', 1)->get());
+    }
+
+    public function showOneTask($id)
+    {
+        $task = Task::findOrFail($id);
+        return response()->json($task, 200);
+    }
+
+    public function addNewTask(Request $request)
+    {
+        Task::create($request->all());
+    }
+
+    public function updateTask($id, Request $request)
+    {
+        $task = Task::findOrFail($id);
+        $task->update($request->all());
+        return response()->json($task, 200);
+    }
+
+    public function deleteTask($id)
+    {
+        $task = Task::findOrFail($id);
+        $task->active(false);
+        $task->save();
+        return response()->json($task->refresh(), 200);
+    }
+
 }
