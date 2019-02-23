126c87a018f34ba0e02ddde92d3e0334e5bba833
George Lucas T. Bentes <george.bentes@gmail.com>
2019-02-23 15:49:51 -0400

    Adding fields to receive values

--- app/Task.php
+++ app/Task.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model 
 {
+  protected $table = 'tasks';
+
+  protected $fillable = [
+    'subject',
+    'description',
+    'remark',
+    'start_date',
+    'expected_end_date',
+    'end_date',
+    'active',
+    'requester_id',
+    'type_id',
+    'status_id',
+    'technician_id'
+  ];
 
 }
